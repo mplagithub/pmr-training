d4f7cab7c8a50ffc09f47e7b89ef4382a14c39da
mplata <mariusz.plata@volvocars.com>
2025-08-16 08:11:39 +0200

    Exercise 4 - minor update

--- pmr/memory_resource.cpp
+++ pmr/memory_resource.cpp
@@ -4,6 +4,14 @@
 #include <new>
 #include <vector>
 
+class A {
+  int x;
+
+ public:
+  A() : x(1) { std::cout << "A constructed\n"; }
+  ~A() { std::cout << "A destructed\n"; }
+};
+
 int main() {
   std::cout << "--- END OF MAIN ---\n";
   return 0;
